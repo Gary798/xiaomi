0e4bf166c3258e8d9099832ffcc26d6ac35a06da
Gary798 <2689685229@qq.com>
2024-04-16 14:10:19 +0800

    用户管理

---
+++ src/main/java/Servlet/upuserServlet.java
@@ -0,0 +1,30 @@
+package Servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import dao.usersdao;
+@WebServlet("/jsp/upbtnSer")
+public class upuserServlet extends HttpServlet{
+	usersdao dao = new usersdao();
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		  request.setCharacterEncoding("utf-8");
+		  int uid = Integer.parseInt(request.getParameter("uid"));
+		  String upname = request.getParameter("upname");
+	      String upphone = request.getParameter("upphone");
+	      String upisadmin = request.getParameter("upisadmin");
+	      int upgender= Integer.parseInt(request.getParameter("upgender"));
+	      int upstatus= Integer.parseInt(request.getParameter("upstatus"));
+	      int isadmin=0;
+	      if (upisadmin!=null) {
+	           isadmin=1;
+	      }
+	      String upsex=upgender==1?"男":"女";
+	      // 在控制台输出接收到的数据，方便调试
+	      dao.updateuser(upname, upphone, upsex, upstatus, isadmin, uid);
+	}
+}
